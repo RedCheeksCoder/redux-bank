c46f8ccd325e3e13d2bf47c00ad35bc6af02202b
Bryan Odiña <149803435+RedCheeksCoder@users.noreply.github.com>
2023-12-15 16:31:27 +0800

    Add files via upload

---
+++ src/features/customers/customerSlice.jsx
@@ -0,0 +1,77 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createdAt;
+
+        console.log(state.fullName);
+        console.log(action.payload.fullName);
+        console.log(action.payload.nationalID);
+      },
+    },
+
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
+
+export const { createCustomer, updateName } = customerSlice.actions;
+export default customerSlice.reducer;
+
+/* const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+export default function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAs: action.payload.createdAs,
+      };
+    case "customer/updateName":
+      return {
+        ...state,
+        fullName: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+export function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createdAs: new Date().toISOString() },
+  };
+}
+
+export function updateName(fullName) {
+  return { type: "customer/updateName", payload: fullName };
+}
+ */
